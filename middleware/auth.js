ed370a696cb27fc4e0e6e9f932550e76a12df553
RYAN CORRIGAN <ryanjcorrigan@lewisu.edu>
2021-12-12 15:33:02 -0600

    added comments, more info on homepage, and popup for non logged in users

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,12 @@
+let alert = require('alert');
+
 module.exports = {
     ensureAuth: function(req, res, next) {
         if(req.isAuthenticated()) {
             return next()
         } else {
             res.redirect('/')
+            alert("Please login to continue");
         }
     }
 }
